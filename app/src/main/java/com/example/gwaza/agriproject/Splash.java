3ae7906aa84403efe8b01a5780c8a8541da97937
AustineGwa <austinegwa64@gmail.com>
2019-03-09 04:48:46 +0200

    added  a splash screen

---
+++ app/src/main/java/com/example/gwaza/agriproject/Splash.java
@@ -0,0 +1,53 @@
+package com.example.gwaza.agriproject;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class Splash extends AppCompatActivity {
+    private  boolean isSessionActive;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash);
+
+        isSessionActive = getSesionStatus().equals("active");
+
+        Thread thread  = new Thread(){
+
+            @Override
+            public  void run(){
+                try {
+                    Thread.sleep(3000);
+                    if(isSessionActive){
+
+                        Intent intent = new Intent(getApplicationContext(),FragmentsHolderActivity.class);
+                        startActivity(intent);
+                        finish();
+
+                    }else {
+                        Intent intent = new Intent(getApplicationContext(), Login.class);
+                        startActivity(intent);
+                        finish();
+                    }
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        thread.start();
+    }
+
+    public String getSesionStatus() {
+
+        return "active";
+    }
+}
